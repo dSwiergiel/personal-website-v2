afbcac90921ac76f19175298a7fdc2458eae9697
Deven Swiergiel <swiergi1441@gmail.com>
2020-04-17 16:36:34 -0400

    more work on landing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Landing from './components/landing/Landing';
-import { Parallax, Background } from 'react-parallax';
+import { Parallax } from 'react-parallax';
 
 import background from './assets/images/landing-background.jpg';
 import './style.scss';
@@ -10,10 +10,10 @@ function App() {
     <div className=''>
       <Parallax
         blur={0}
-        bgImage={require('./assets/images/landing-background.jpg')}
-        bgImageAlt='the cat'
+        bgImage={background}
+        bgImageAlt='Deven Swiergiel'
         strength={600}
-        className='pb-5'
+        className='pb-5 '
       >
         <div style={{ height: '150px' }} />
         <Landing></Landing>
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -4,12 +4,23 @@ import './landing.scss';
 
 const Landing = () => {
   return (
-    <div className='container'>
-      <div className='landing-box'>
+    <div className='container landing-box'>
+      <div className='row px-2'>
         <div className='col-md-4'>
           <img src={portrait} className='portrait' alt='Deven Swiergiel'></img>
         </div>
-        <div className='col-md-8'></div>
+        <div className='col-md-8'>
+          <p>
+            My name is Deven Swiergiel and I like to make stuff. I currently
+            work full-time for the government as a Full-Stack Software
+            Engineer/Developer. I have experience working on a team as well as
+            by myself creating and deploying enterprise software and systems for
+            New York State. I enjoy learning new skills and love creating even
+            more. This website was designed and created by me. If you'd like,
+            explore it to learn more about me!
+          </p>
+          <button className='slide-up-button'>Contact Me</button>
+        </div>
       </div>
     </div>
   );
