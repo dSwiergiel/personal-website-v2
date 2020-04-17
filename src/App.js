2ed65ff597c34fa2aa38f775c158b3b51d528ac6
Deven Swiergiel <swiergi1441@gmail.com>
2020-04-17 12:17:53 -0400

    clean up code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import './style.scss';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1>
-          Edit <code>src/App.js</code> and save to reload.
-        </h1>
-
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <div></div>
     </div>
   );
 }
