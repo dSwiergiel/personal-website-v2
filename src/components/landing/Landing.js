a459630037bc2a52b1a3d27d3665399a02ca0909
Deven Swiergiel <swiergi1441@gmail.com>
2020-04-17 20:32:52 -0400

    partial landing text and buttons

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -6,20 +6,34 @@ const Landing = () => {
   return (
     <div className='container landing-box'>
       <div className='row px-2'>
-        <div className='col-md-4'>
+        <div className='col-lg-4 '>
           <img src={portrait} className='portrait' alt='Deven Swiergiel'></img>
         </div>
-        <div className='col-md-8'>
-          <p>
-            My name is Deven Swiergiel and I like to make stuff. I currently
-            work full-time for the government as a Full-Stack Software
-            Engineer/Developer. I have experience working on a team as well as
-            by myself creating and deploying enterprise software and systems for
-            New York State. I enjoy learning new skills and love creating even
-            more. This website was designed and created by me. If you'd like,
-            explore it to learn more about me!
+        <div className='col-lg-8'>
+          <p className='light-text'>
+            My name is Deven Swiergiel and I like to make stuff. Currently
+            working for the government as a Full-Stack Software Developer with
+            emphasis on the front end. I have experience working on a team as
+            well as by myself creating and deploying enterprise software and
+            systems for New York State.
           </p>
-          <button className='slide-up-button'>Contact Me</button>
+          <p className='light-text'>
+            This website was designed and created in React by me as a V2. The
+            pervious version was made with no frameworks while I was first
+            learning web development, and while it may have looked good on the
+            outside, it was an absolute dumpster fire on the inside!
+          </p>
+          <p className='light-text mb-0'>
+            Look around a bit to learn more about me and the projects I've
+            worked on.
+          </p>
+          <div className='form-row align-items-center'>
+            <button className='slide-up-button mt-3 mr-3'>Contact Me</button>
+            <button className='slide-up-button mt-3 mr-3'>Resume</button>
+            <a>
+              <i className='fab fa-github landing-fontawesome mt-3'></i>
+            </a>
+          </div>
         </div>
       </div>
     </div>
