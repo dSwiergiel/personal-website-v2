fe1d8e55febf2ac7889be1c6d694fd62290315d8
Deven Swiergiel <devenswiergiel@gmail.com>
2020-04-29 11:49:50 -0400

    Mobile enhancements

--- src/App.js
+++ src/App.js
@@ -1,52 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import Landing from './components/landing/Landing';
-import SplashScreen from './components/splash/SplashScreen';
+// import SplashScreen from './components/splash/SplashScreen';
 import Parallax from './components/parallax/Parallax';
 import ParallaxMobile from './components/parallax/ParallaxMobile';
 import LazyLoad from 'react-lazyload';
 
 import logo from './assets/images/react-logo.svg';
 import background from './assets/images/landing-background.jpg';
-import mobileBackground from './assets/images/el-capitan-5k.jpg';
-// import mobileBackground from './assets/images/yosemite-5k.jpg';
+// import mobileBackground from './assets/images/el-capitan-5k.jpg';
+import mobileBackground from './assets/images/yosemite-5k.jpg';
 
 function App() {
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // setTimeout(() => {
-    //   setLoading(true);
-    // }, 1);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
     //eslint-disable-next-line
   }, []);
   return (
     <div>
-      <div className='splash-screen'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p className='light-text fixed-bottom text-center h-25'>
-          getting things together...
-        </p>
-      </div>
-      <div className='fade-in-from-black'></div>
-      {/* <LazyLoad once={true}>
-        <Parallax background={background} speed={0.5} alt='landing backgound'>
-          REMOVE ME
-          <div style={{ height: '125px' }} />
-          REMOVE ME
-          <div className='container py-5'>
-            <Landing></Landing>
-          </div>
-        </Parallax>
-      </LazyLoad> */}
-      <ParallaxMobile
-        background={mobileBackground}
-        strength={1000}
-        blur={{ min: -10, max: 20 }}
-      >
-        <div className='container py-3 '>
-          <Landing></Landing>
+      {/* Splash Screen */}
+      {loading && (
+        <div className='splash-screen'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p className='light-text fixed-bottom text-center h-25'>
+            getting things together...
+          </p>
         </div>
-      </ParallaxMobile>
+      )}
+      {/* Fade out from black */}
+      <div className='fade-in-from-black'></div>
+      {/* Use different parallax for mobile screens vs desktop */}
+      {window.innerWidth > 768 ? (
+        <LazyLoad once={true}>
+          <Parallax background={background} speed={0.5} alt='landing backgound'>
+            REMOVE ME
+            <div style={{ height: '125px' }} />
+            REMOVE ME
+            <div className='container py-5'>
+              <Landing></Landing>
+            </div>
+          </Parallax>
+        </LazyLoad>
+      ) : (
+        <LazyLoad once={true}>
+          <ParallaxMobile
+            background={mobileBackground}
+            strength={1000}
+            blur={{ min: -10, max: 20 }}
+          >
+            <div className='container py-3 '>
+              <Landing></Landing>
+            </div>
+          </ParallaxMobile>
+        </LazyLoad>
+      )}
       <div style={{ height: '200px' }} /> <div style={{ height: '200px' }} />{' '}
       <div style={{ height: '200px' }} /> <div style={{ height: '200px' }} />{' '}
       <div style={{ height: '200px' }} /> <div style={{ height: '200px' }} />{' '}
@@ -54,5 +64,5 @@ function App() {
   );
 }
 
-// export default App;
-export default SplashScreen(App);
+export default App;
+// export default SplashScreen(App);
