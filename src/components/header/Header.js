d5979b2c2f2d0956e15f53f285d6ee102d2e4e95
Deven Swiergiel <devenswiergiel@gmail.com>
2020-05-01 14:47:23 -0400

    header work

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './header.scss';
 
 const Header = () => {
+  let header = useRef(null);
+  let sticky;
+  useEffect(() => {
+    sticky = header.offsetTop;
+    //eslint-disable-next-line
+  }, []);
+  //   window.onscroll = function () {
+  //     myFunction();
+  //   };
+
+  //   function myFunction() {
+  //     console.log('Im scrolling');
+  //     if (window.pageYOffset > sticky) {
+  //       header.classList.add('sticky');
+  //     } else {
+  //       header.classList.remove('sticky');
+  //     }
+  //   }
   return (
-    <div className='container header-container py-4'>
+    <div
+      className='container header-container py-4'
+      id='header'
+      ref={(el) => {
+        header = el;
+      }}
+    >
       <h1 className='header'>
         Deven
         <br />
