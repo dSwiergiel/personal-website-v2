29dd2508207803207669fc604ba3988f0cc21bfa
Deven Swiergiel <devenswiergiel@gmail.com>
2020-06-22 12:32:45 -0400

    clean up

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 // import LazyLoad from 'react-lazyload';
 
 import Splash from '../splash/Splash';
