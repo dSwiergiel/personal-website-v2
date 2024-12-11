8c185271e020ce82dcdff931a3cdf4660cea12b9
Deven Swiergiel <devenswiergiel@gmail.com>
2024-12-11 16:04:27 -0500

    Clean up eslint warnings and updates resume

--- src/components/main/projects/SimpleSlider.js
+++ src/components/main/projects/SimpleSlider.js
@@ -1,23 +1,10 @@
-import React from 'react';
-import Slider from 'react-slick';
-import useWindowDimensions from '../../../hooks/WindowDimensions';
-
-function CustomPrevArrow(props) {
-  const { onClick } = props;
-  return (
-    <i className='customPrevArrow fas fa-chevron-left' onClick={onClick}></i>
-  );
-}
-function CustomNextArrow(props) {
-  const { onClick } = props;
-  return (
-    <i className='customNextArrow fas fa-chevron-right' onClick={onClick}></i>
-  );
-}
+import React from "react";
+import Slider from "react-slick";
+import useWindowDimensions from "../../../hooks/WindowDimensions";
 
 export const SimpleSlider = ({
   settings = {
-    className: '',
+    className: "",
     dots: true,
     infinite: true,
     lazyLoad: true,
@@ -28,9 +15,6 @@ export const SimpleSlider = ({
     autoplay: false,
     autoplaySpeed: 3000,
     pauseOnHover: true,
-    // prevArrow: <CustomPrevArrow />,
-    // nextArrow: <CustomNextArrow />,
-    // customPaging: (i) => <i class='fas fa-minus'></i>,
   },
   project,
   isThumbnail = false,
@@ -38,26 +22,26 @@ export const SimpleSlider = ({
 }) => {
   const { width } = useWindowDimensions();
   return (
-    <div className={isThumbnail ? '' : 'container pb-3'}>
+    <div className={isThumbnail ? "" : "container pb-3"}>
       <Slider {...settings}>
         {project.images.map((image) => (
           <div
             key={image}
-            role={isThumbnail ? 'button' : ''}
+            role={isThumbnail ? "button" : ""}
             onClick={onOpenModal}
           >
             <img
               alt={image.header}
-              className='project-image'
-              style={{ maxHeight: isThumbnail ? '175px' : '' }}
+              className="project-image"
+              style={{ maxHeight: isThumbnail ? "175px" : "" }}
               src={require(`../../../assets/images/projects/${image.filename}`)}
             />
             {!isThumbnail && (
-              <div className='project-image-caption pt-3  d-flex align-items-center'>
+              <div className="project-image-caption pt-3  d-flex align-items-center">
                 {width > 991 ? (
-                  <p className='mx-auto mb-0'>{image.description}</p>
+                  <p className="mx-auto mb-0">{image.description}</p>
                 ) : (
-                  <small className='mx-auto'>{image.description}</small>
+                  <small className="mx-auto">{image.description}</small>
                 )}
               </div>
             )}
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,16 +1,16 @@
-import { SpeedInsights } from "@vercel/speed-insights/react"
-import React, { useState, useEffect, lazy, Suspense } from 'react';
-import Splash from '../main/splash/Splash';
-import Nav from '../main/nav/Nav';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import React, { useState, useEffect, lazy, Suspense } from "react";
+import Splash from "../main/splash/Splash";
+import Nav from "../main/nav/Nav";
 
-import LazyRender from '../wrapers/lazy-render/LazyRender';
+import LazyRender from "../wrapers/lazy-render/LazyRender";
 
-const Landing = lazy(() => import('../main/landing/Landing'));
-const About = lazy(() => import('../main/about/About'));
-const Stack = lazy(() => import('../main/stack/Stack'));
-const Projects = lazy(() => import('../main/projects/Projects'));
-const Contact = lazy(() => import('../main/contact/Contact'));
-const Footer = lazy(() => import('../main/footer/Footer'));
+const Landing = lazy(() => import("../main/landing/Landing"));
+const About = lazy(() => import("../main/about/About"));
+const Stack = lazy(() => import("../main/stack/Stack"));
+const Projects = lazy(() => import("../main/projects/Projects"));
+// const Contact = lazy(() => import('../main/contact/Contact'));
+const Footer = lazy(() => import("../main/footer/Footer"));
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -24,36 +24,35 @@ function Home() {
 
   return (
     <div>
-      <SpeedInsights/>
+      <SpeedInsights />
       {/* Splash Screen */}
       {loading && <Splash duration={loadingTime}></Splash>}
       <Nav duration={loadingTime}></Nav>
       <main>
-      <div className='pb-5 container content' name='landing'>
-        <div class="landing-bg"></div>
-        <Landing></Landing>
-      </div>
+        <div className="pb-5 container content" name="landing">
+          <div class="landing-bg"></div>
+          <Landing></Landing>
+        </div>
 
-      <Suspense fallback={<h1>Still Loading…</h1>}>
-        <div class="bg-white">
-          <div className='container  py-4 px-4'>
-            <div className='row'>
-              <div name='about' className='col-lg-6'>
-                <About></About>
-              </div>
-              <div name='stack' className='col-lg-6'>
-                <Stack></Stack>
+        <Suspense fallback={<h1>Still Loading…</h1>}>
+          <div class="bg-white">
+            <div className="container  py-4 px-4">
+              <div className="row">
+                <div name="about" className="col-lg-6">
+                  <About></About>
+                </div>
+                <div name="stack" className="col-lg-6">
+                  <Stack></Stack>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-
-      </Suspense>
-      <Suspense fallback={<h1>Still Loading…</h1>}>
-        <div className='bg-warning pt-4' name='projects'>
-          <Projects></Projects>
-        </div>
-      </Suspense>
+        </Suspense>
+        <Suspense fallback={<h1>Still Loading…</h1>}>
+          <div className="bg-warning pt-4" name="projects">
+            <Projects></Projects>
+          </div>
+        </Suspense>
       </main>
       <Suspense fallback={<h1>Still Loading…</h1>}>
         <div class="bg-white">
